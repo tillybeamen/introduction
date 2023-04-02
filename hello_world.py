cb2076692419355f8ac79d1ca978563d0074ad36
Tillman Pugh <Till.pugh@gmail.com>
2023-04-02 13:21:14 -0700

    first commit

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+
+
+#1
+
+print("Hello World")
+
+#2
+
+name = "Tillman"
+print(f"Hello {name}")
+print("Hello " + name)
+print("Hello",name)
+
+#3
+
+name = 42
+print("Hello" , name)
+print("Hello " + str(name))
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
+
+
