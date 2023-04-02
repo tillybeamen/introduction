7d9d808e3cea5cfc89eb62ced85c0f8338fac0f2
Tillman Pugh <Till.pugh@gmail.com>
2023-04-02 13:23:48 -0700

    first commit

---
+++ testing_python.py
@@ -0,0 +1,92 @@
+# import random
+
+# print('Welcome to Python!')
+
+# print('This is a loop printing 5 times')
+# for x in range(1, 6):
+#     print(f'x is: {x}')
+
+# weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+# day = random.choice(weekdays)
+
+# print(f'Today is: {day}')
+
+# if day == 'Monday':
+#     print('It will be a long week!')
+# elif(day == 'Friday'):
+#     print('Woohoo, time for the weekend!')
+# else:
+#     print('Not quite there yet.')
+
+
+
+
+
+
+
+
+# x = 10
+# if x > 50:
+#     print("bigger than 50")
+# else:
+#     print("smaller than 50")
+
+
+
+
+
+
+
+# import random
+# print(random.randint(2,5)) # provides a random number between 2 and 5
+
+
+
+
+
+
+# total = 33
+# total_plus = "42"
+# total = total + int(total_plus)
+# print(total)
+
+
+
+
+
+# first_name = "Tillman"
+# last_name = "Pugh"
+# age = 34
+# print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+
+
+
+# first_name = "Alana"
+# last_name = "James"
+# age = 23
+# profession = "Store clerk"
+# years_experience = 8
+
+# greeting = "Hello my name is ", first_name, last_name
+
+# print(greeting)
+
+# print(f"I am {age} yeears old")
+
+# print("I work as a {}.".format(profession))
+
+# exp_string = "I have worked in the field for {} years."
+# print(exp_string.format(years_experience))
+
+# print("I started in the field when I was " + str(age) + " years old.")
+
+
+
+
+
+
+
+
+
+
